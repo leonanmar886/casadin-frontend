e27992123e008b21f9bbb5ac693977ccefaef096
Kauan Soares <gbskauan@gmail.com>
2025-06-25 22:08:51 -0300

    feat: create FianceBanner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 import Form from '@/components/Form';
-import FiancePhoto from "@/components/FiancePhoto";
 import Button from "@/components/Button";
+import Product from '@/components/Product';
+import FiancePhoto from "@/components/FiancePhoto";
 import BestManPhoto from "@/components/BestManPhoto";
 import CardMarriage from "@/components/CardMarriage";
-import Product from '@/components/Product';
+import FianceBanner from '@/components/FianceBanner';
 import airfryerImage from '@/components/airfryer.png';
 
 export default function Home() {
@@ -13,11 +14,13 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <FianceBanner></FianceBanner>
         <Button text="Entrar"></Button>
         <Form text="E-mail"/>
         <FiancePhoto text="Noivo 1"></FiancePhoto>
         <BestManPhoto text="Padrinho"></BestManPhoto>
         <CardMarriage text="Noivo&Noivo"></CardMarriage>
+
         <Product
           id={1}
           name="Air Fryer"
---
+++ src/components/FianceBanner.tsx
@@ -0,0 +1,14 @@
+// src/app/components/FiancePhoto.tsx
+import React from 'react';
+import './FianceBanner.css'; // O arquivo CSS para estilização
+
+const FianceBanner = () => {
+
+  return (
+    <div className="fiance-banner-container">
+      <div className="fiances-photo"></div>
+    </div>
+  );
+};
+
+export default FianceBanner;
